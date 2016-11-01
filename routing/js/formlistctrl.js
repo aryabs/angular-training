d8b035e1ab96d207858dc3a5bf4c5b091cdacc69
arya <bsaryaskumar@gmail.com>
2016-11-01 17:29:36 +0530

    search&sort

--- routing/js/formlistctrl.js
+++ routing/js/formlistctrl.js
@@ -1,12 +1,37 @@
+
+
+// controlling the contactlist page
 angular.module("myApp")
-.controller('FormListController',function($scope){
+.controller('FormListController',function($scope)
+{
 	
 	function getData(){
-		var dataArray = localStorage.getItem('data');
-		dataArray = dataArray ? JSON.parse(dataArray) : [];
+		// getting data from local storage
+		var dataArray= localStorage.getItem('data');
+		dataArray= dataArray ? JSON.parse(dataArray) : [];
 		$scope.dataArray = dataArray;
 
 	}
-	getData();
+	//populating dataarray
 
-	});
+	getData();
+	
+	// sorting function
+  	$scope.sort=function(param)	{ 
+	 	
+	  	$scope.orderByField = param;	
+	  	$scope.reverseSort = !$scope.reverseSort;
+	  	
+	  	
+  	}
+
+// app.filter('titleCase', function () {
+//   return function (item) {
+//   	item =$scope.firstName;
+//   	console.log(item);
+//     // return item.toUpperCase();
+  // };
+
+// });
+
+});
