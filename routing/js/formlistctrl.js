b4397ab84190d4530684711923427c1b30e568b9
aryabs <bsaryaskumar@gmail.com>
2016-11-02 12:09:10 +0530

    edited

--- routing/js/formlistctrl.js
+++ routing/js/formlistctrl.js
@@ -1,37 +1,27 @@
 
-
 // controlling the contactlist page
 angular.module("myApp")
 .controller('FormListController',function($scope)
-{
-	
-	function getData(){
+		{	
+		function getData(){
 		// getting data from local storage
-		var dataArray= localStorage.getItem('data');
-		dataArray= dataArray ? JSON.parse(dataArray) : [];
-		$scope.dataArray = dataArray;
-
-	}
-	//populating dataarray
-
-	getData();
-	
-	// sorting function
-  	$scope.sort=function(param)	{ 
-	 	
-	  	$scope.orderByField = param;	
-	  	$scope.reverseSort = !$scope.reverseSort;
-	  	
-	  	
-  	}
-
-
+				var dataArray= localStorage.getItem('data');
+				dataArray= dataArray ? JSON.parse(dataArray) : [];
+				$scope.dataArray = dataArray;
+		}
+		//populating dataarray
+ 		getData();
+		// sorting function
+	  	$scope.sort=function(param)	{ 	
+		  	$scope.orderByField = param;	
+		  	$scope.reverseSort = !$scope.reverseSort;	
+	  	}
 });
-
+// definition of titlecase filter
 angular.module("myApp").filter('titleCase', function() {
-    return function(input) {
-      input = input;
-      return input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+	    return function(input) {
+	    input = input;
+	    return input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
       
   }
 });
