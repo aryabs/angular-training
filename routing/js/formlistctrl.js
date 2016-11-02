e939835cf43a98741df96cf08aa35af76a059f01
aryabs <bsaryaskumar@gmail.com>
2016-11-02 10:57:48 +0530

    customfilter

--- routing/js/formlistctrl.js
+++ routing/js/formlistctrl.js
@@ -25,13 +25,13 @@ angular.module("myApp")
 	  	
   	}
 
-// app.filter('titleCase', function () {
-//   return function (item) {
-//   	item =$scope.firstName;
-//   	console.log(item);
-//     // return item.toUpperCase();
-  // };
 
-// });
+});
 
+angular.module("myApp").filter('titleCase', function() {
+    return function(input) {
+      input = input;
+      return input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+      
+  }
 });
